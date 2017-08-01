8fa8b420ae62df42309f2301e6374352ebaa0179
NicoCroce <ingcrocenicolas@outlook.com>
2017-07-31 22:19:15 -0300

    FIREBASE - Se modificó la ref de firebase.

--- firebase.js
+++ firebase.js
@@ -4,11 +4,11 @@ var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://chabashoytest.firebaseio.com"
+    databaseURL: "https://chabashoy-2c020.firebaseio.com"
 });
 
 var db = admin.database();
-var ref = db.ref("clima");
+var ref = db.ref("data/clima");
 
 function sendData(data) {
     /* var newTelKey = firebase.database().ref('data/servicios/utiles').push().key; */
