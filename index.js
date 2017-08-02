6c6707f4251a813b88e11b59b6edbb879a695394
NicoCroce <ingcrocenicolas@outlook.com>
2017-08-01 22:49:05 -0300

    Se usa Express

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const cors = require('cors')({ origin: true });
 const getPage = require('./getPage.js');
 const firebase = require('./firebase.js');
 
-var http = require("http");
-setInterval(function () {
-  http.get("https://chabashoy.herokuapp.com/");
-  console.log('Despertando');
-}, 300000); // every 5 minutes (300000)
-
 var app = express();
 app.set('port', (process.env.PORT || 2173));
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ' + app.get('port'));
 });
+
+setInterval(function () {
+  app.get("https://chabashoy.herokuapp.com/");
+  console.log('Despertando');
+}, 300000); // every 5 minutes (300000)
 
 app.get('/', function (req, res) {
   cors(req, res, () => {
